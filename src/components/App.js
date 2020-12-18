d04b88c7717e0fc8637eaf0670296e97e4889e74
Jose <josecaramor1@hotmail.com>
2020-12-18 22:30:14 +0100

    leftbar created

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Leftbar from './Leftbar';
+
+class App extends React.Component{
+    render(){
+        return (
+            <Leftbar />
+        );
+    }
+}
+
+export default App;
